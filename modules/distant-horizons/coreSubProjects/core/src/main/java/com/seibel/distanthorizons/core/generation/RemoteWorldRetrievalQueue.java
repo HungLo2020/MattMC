d685dad9eaadb5165606fae95f4fe6259c4de584
matt <mcsherfey@yahoo.com>
2025-12-19 15:51:55 -0800

    added dh

---
+++ modules/distant-horizons/coreSubProjects/core/src/main/java/com/seibel/distanthorizons/core/generation/RemoteWorldRetrievalQueue.java
@@ -0,0 +1,165 @@
+package com.seibel.distanthorizons.core.generation;
+
+import com.seibel.distanthorizons.core.config.Config;
+import com.seibel.distanthorizons.core.generation.tasks.IWorldGenTaskTracker;
+import com.seibel.distanthorizons.core.generation.tasks.WorldGenResult;
+import com.seibel.distanthorizons.core.level.DhClientLevel;
+import com.seibel.distanthorizons.core.logging.DhLoggerBuilder;
+import com.seibel.distanthorizons.core.multiplayer.client.AbstractFullDataNetworkRequestQueue;
+import com.seibel.distanthorizons.core.multiplayer.client.ClientNetworkState;
+import com.seibel.distanthorizons.core.pos.DhSectionPos;
+import com.seibel.distanthorizons.core.pos.blockPos.DhBlockPos2D;
+import com.seibel.distanthorizons.core.render.renderer.IDebugRenderable;
+import com.seibel.distanthorizons.core.util.LodUtil;
+import com.seibel.distanthorizons.core.util.WorldGenUtil;
+import com.seibel.distanthorizons.core.util.objects.RollingAverage;
+import com.seibel.distanthorizons.core.logging.DhLogger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.*;
+
+public class RemoteWorldRetrievalQueue extends AbstractFullDataNetworkRequestQueue implements IFullDataSourceRetrievalQueue, IDebugRenderable
+{
+	private static final DhLogger LOGGER = new DhLoggerBuilder().build();
+	
+	private int estimatedRemainingTaskCount;
+	private int estimatedTotalChunkCount;
+	
+	private final RollingAverage rollingAverageChunkGenTimeInMs = new RollingAverage(1_000);
+	@Override public RollingAverage getRollingAverageChunkGenTimeInMs() { return this.rollingAverageChunkGenTimeInMs; }
+	
+	
+	
+	//=============//
+	// constructor //
+	//=============//
+	
+	public RemoteWorldRetrievalQueue(ClientNetworkState networkState, DhClientLevel level)
+	{ super(networkState, level, false, Config.Client.Advanced.Debugging.DebugWireframe.showWorldGenQueue); }
+	
+	
+	
+	//===========================//
+	// retrieval queue overrides //
+	//===========================//
+	
+	@Override
+	public void startAndSetTargetPos(DhBlockPos2D targetPos) { super.tick(targetPos); }
+	
+	@Override
+	public byte lowestDataDetail() { return LodUtil.BLOCK_DETAIL_LEVEL + 12; } // TODO should be the same as what the server's update propagator can provide
+	@Override
+	public byte highestDataDetail() { return LodUtil.BLOCK_DETAIL_LEVEL; }
+	
+	@Override
+	public CompletableFuture<WorldGenResult> submitRetrievalTask(long sectionPos, byte requiredDataDetail, IWorldGenTaskTracker tracker)
+	{
+		long generationStartMsTime = System.currentTimeMillis();
+		
+		
+		return super.submitRequest(sectionPos, fullDataSource -> {
+					Objects.requireNonNull(tracker.getDataSourceConsumer()).accept(fullDataSource);
+					fullDataSource.close();
+				})
+				.thenApply(requestResult ->
+				{
+					long totalGenTimeInMs = System.currentTimeMillis() - generationStartMsTime;
+					
+					int chunkWidth = DhSectionPos.getChunkWidth(sectionPos);
+					int chunkCount = chunkWidth * chunkWidth;
+					double timePerChunk = (double)totalGenTimeInMs / (double)chunkCount;
+					this.rollingAverageChunkGenTimeInMs.add(timePerChunk);
+					
+					switch (requestResult)
+					{
+						case SUCCEEDED:
+							return WorldGenResult.CreateSuccess(sectionPos);
+						case FAILED:
+							return WorldGenResult.CreateFail();
+						case REQUIRES_SPLITTING:
+							List<CompletableFuture<WorldGenResult>> childFutures = new ArrayList<>(4);
+							DhSectionPos.forEachChild(sectionPos, childPos -> {
+								tracker.shouldGenerateSplitChild(childPos).thenAccept(shouldGenerate -> {
+									if (shouldGenerate)
+									{
+										childFutures.add(this.submitRetrievalTask(childPos, requiredDataDetail, tracker));
+									}
+								});
+							});
+							return WorldGenResult.CreateSplit(childFutures);
+					}
+					
+					LodUtil.assertNotReach("Unexpected and unhandled request response result: ["+requestResult+"]");
+					return WorldGenResult.CreateFail();
+				});
+	}
+	
+	@Override
+	public CompletableFuture<Void> startClosingAsync(boolean cancelCurrentGeneration, boolean alsoInterruptRunning)
+	{ return super.startClosingAsync(alsoInterruptRunning); }
+	
+	
+	
+	//=================================//
+	// network request queue overrides //
+	//=================================//
+	
+	@Override
+	protected int getRequestRateLimit() { return this.networkState.sessionConfig.getGenerationRequestRateLimit(); }
+	@Override
+	protected boolean isSectionAllowedToGenerate(long sectionPos, DhBlockPos2D targetPos)
+	{
+		if (this.networkState.sessionConfig.getGenerationMaxChunkRadius() > 0)
+		{
+			boolean posInRange = WorldGenUtil.isPosInWorldGenRange(
+				sectionPos,
+				this.networkState.sessionConfig.getGenerationCenterChunkX(), this.networkState.sessionConfig.getGenerationCenterChunkZ(),
+				this.networkState.sessionConfig.getGenerationMaxChunkRadius()
+				);
+			if (!posInRange)
+			{
+				return false;
+			}
+		}
+		
+		return DhSectionPos.getChebyshevSignedBlockDistance(sectionPos, targetPos) <= this.networkState.sessionConfig.getMaxGenerationRequestDistance() * 16;
+	}
+	@Override
+	protected boolean onBeforeRequest(long sectionPos, CompletableFuture<ERequestResult> future)
+	{
+		if (DhSectionPos.getDetailLevel(sectionPos) > DhSectionPos.SECTION_MINIMUM_DETAIL_LEVEL
+				&& !Config.Server.Experimental.enableNSizedGeneration.get())
+		{
+			future.complete(ERequestResult.REQUIRES_SPLITTING);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	protected String getQueueName() { return "World Remote Generation Queue"; }
+	
+	
+	
+	//===============//
+	// debug display //
+	//===============//
+	
+	@Override
+	public int getEstimatedRemainingTaskCount() { return this.estimatedRemainingTaskCount; }
+	@Override
+	public void setEstimatedRemainingTaskCount(int newEstimate) { this.estimatedRemainingTaskCount = newEstimate; }
+	
+	@Override
+	public int getRetrievalEstimatedRemainingChunkCount() { return this.estimatedTotalChunkCount; }
+	@Override
+	public void setRetrievalEstimatedRemainingChunkCount(int newEstimate) { this.estimatedTotalChunkCount = newEstimate; }
+	
+	@Override 
+	public int getQueuedChunkCount() { return 0; }
+	
+	
+}
---
+++ modules/distant-horizons/coreSubProjects/core/src/main/java/com/seibel/distanthorizons/core/world/IDhWorld.java
@@ -0,0 +1,43 @@
+/*
+ *    This file is part of the Distant Horizons mod
+ *    licensed under the GNU LGPL v3 License.
+ *
+ *    Copyright (C) 2020 James Seibel
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, version 3.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.seibel.distanthorizons.core.world;
+
+import com.seibel.distanthorizons.core.level.IDhLevel;
+import com.seibel.distanthorizons.core.wrapperInterfaces.world.ILevelWrapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.Closeable;
+import java.util.concurrent.CompletableFuture;
+
+// TODO why is this exist alongside AbstractDhWorld?
+public interface IDhWorld extends Closeable
+{
+	
+	@Nullable
+	IDhLevel getOrLoadLevel(@NotNull ILevelWrapper levelWrapper);
+	@Nullable
+	IDhLevel getLevel(@NotNull ILevelWrapper wrapper);
+	Iterable<? extends IDhLevel> getAllLoadedLevels();
+	int getLoadedLevelCount();
+	
+	void unloadLevel(@NotNull ILevelWrapper levelWrapper);
+	
+}
