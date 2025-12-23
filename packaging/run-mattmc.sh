01acb50297e528cb3e1cbed8501d21a81502d89d
matt <mcsherfey@yahoo.com>
2025-12-23 13:04:20 -0800

    clean up

--- packaging/run-mattmc.sh
+++ packaging/run-mattmc.sh
@@ -18,24 +18,9 @@ fi
 # Path to the Minecraft game JAR for Fabric Loader
 GAME_JAR="${SCRIPT_DIR}/lib/minecraft-@VERSION@.jar"
 
-"$JAVA_CMD" -Xmx2G -Xms512M \
-    -XX:+UseG1GC \
-    -XX:+ParallelRefProcEnabled \
-    -XX:MaxGCPauseMillis=200 \
-    -XX:+UnlockExperimentalVMOptions \
-    -XX:+DisableExplicitGC \
-    -XX:G1NewSizePercent=30 \
-    -XX:G1MaxNewSizePercent=40 \
-    -XX:G1HeapRegionSize=8M \
-    -XX:G1ReservePercent=20 \
-    -XX:G1HeapWastePercent=5 \
-    -XX:G1MixedGCCountTarget=4 \
-    -XX:InitiatingHeapOccupancyPercent=15 \
-    -XX:G1MixedGCLiveThresholdPercent=90 \
-    -XX:G1RSetUpdatingPauseTimePercent=5 \
-    -XX:SurvivorRatio=32 \
-    -XX:+PerfDisableSharedMem \
-    -XX:MaxTenuringThreshold=1 \
+"$JAVA_CMD" -Xmx8G -Xms4G \
+    -XX:+UseZGC \
+    -XX:+ZGenerational \
     -Dfabric.gameJarPath.client="${GAME_JAR}" \
     -Dfabric.development=true \
     -cp "@CLASSPATH_LINUX@" \
