a4a4f6a11c531c7e5fa6ee424d6e69acc99ed6c1
matt <mcsherfey@yahoo.com>
2025-12-29 13:35:19 -0800

    updated error log

--- modules/distant-horizons/coreSubProjects/core/src/main/java/com/seibel/distanthorizons/core/network/event/internal/CloseInternalEvent.java
+++
@@ -1,9 +0,0 @@
-package com.seibel.distanthorizons.core.network.event.internal;
-
-/**
- * This event is used to indicate a disconnect.
- */
-public class CloseInternalEvent extends AbstractInternalEvent
-{
-	
-}
