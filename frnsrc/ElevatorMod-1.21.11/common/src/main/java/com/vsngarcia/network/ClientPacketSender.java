384fbd9c9ddeaafa678521064739cffc6d2e7422
matt <mcsherfey@yahoo.com>
2026-01-10 17:15:49 -0800

    clean up

--- frnsrc/ElevatorMod-1.21.11/common/src/main/java/com/vsngarcia/network/ClientPacketSender.java
+++
@@ -1,7 +0,0 @@
-package com.vsngarcia.network;
-
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-
-public interface ClientPacketSender {
-    void sendToServer(CustomPacketPayload packet);
-}
