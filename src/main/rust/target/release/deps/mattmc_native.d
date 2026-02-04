fbd65c0134539d980d365688fe353b396d5c31ec
matt <mcsherfey@yahoo.com>
2026-02-03 22:50:12 -0800

    just cleanup

--- src/main/rust/target/release/deps/mattmc_native.d
+++
@@ -1,5 +0,0 @@
-/home/runner/work/MattMC/MattMC/src/main/rust/target/release/deps/mattmc_native.d: src/lib.rs
-
-/home/runner/work/MattMC/MattMC/src/main/rust/target/release/deps/libmattmc_native.so: src/lib.rs
-
-src/lib.rs:
--- src/main/rust/target/release/libmattmc_native.d
+++
@@ -1,1 +0,0 @@
-/home/runner/work/MattMC/MattMC/src/main/rust/target/release/libmattmc_native.so: /home/runner/work/MattMC/MattMC/src/main/rust/src/lib.rs
