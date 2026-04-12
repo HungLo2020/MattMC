d004dd645beeb858cd550d14e7b05d006f199b12
matt <mcsherfey@yahoo.com>
2026-04-12 15:51:39 -0700

    map player icon fixed. validated

--- src/main/java/net/voxelmap/persistent/GuiPersistentMap.java
+++ src/main/java/net/voxelmap/persistent/GuiPersistentMap.java
@@ -799,7 +799,7 @@ public class GuiPersistentMap extends PopupGuiScreen implements IGuiWaypoints {
             guiGraphics.pose().rotate(locate);
             guiGraphics.pose().translate(-x, -y);
 
-            VoxelMapGuiGraphics.blitFloat(guiGraphics, VoxelMapPipelines.GUI_TEXTURED_LESS_OR_EQUAL_DEPTH_PIPELINE, voxelmapSkinLocation, x - width / 2, y - height / 2, width, height, 0, 1, 0, 1, 0xFFFFFFFF);
+            VoxelMapGuiGraphics.blitFloat(guiGraphics, RenderPipelines.GUI_TEXTURED, voxelmapSkinLocation, x - width / 2, y - height / 2, width, height, 0, 1, 0, 1, 0xFFFFFFFF);
 
             guiGraphics.pose().popMatrix();
         }
